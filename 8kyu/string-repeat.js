d18ab6e9e3c14085c5637b9ff1a88c9951de9e0e
Alex Galygo <alex.galygo.94@gmail.com>
2023-11-28 18:58:23 +0300

    Add string repeat function.
    In this commit, a new function 'repeatStr' is added. This function receives two parameters 'n' (an integer) and 's' (a string) and returns the string 's' repeated 'n' times. Considering this function can be used in various scenarios where a string needs to be repeated, it is useful to have this functionality incorporated in one function.

---
+++ 8kyu/string-repeat.js
@@ -0,0 +1,20 @@
+// Description:
+//Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+//
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+// function repeatStr (n, s) {
+//   return '';
+// }
+
+// Solution:
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+
+// Tests:
+console.log(repeatStr(6, "I"));
+console.log(repeatStr(5, "Hello"));
