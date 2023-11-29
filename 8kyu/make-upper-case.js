e6a8b14290cd6c10d5d827660af455b9f64e7cb8
Alex Galygo <alex.galygo.94@gmail.com>
2023-11-29 11:47:13 +0300

    Implement uppercase conversion function
    Added a new file `make-upper-case.js` in `8kyu` directory. This file contains a function that converts the input string to uppercase. It will be useful for various string manipulation tasks requiring uppercase transformation.

---
+++ 8kyu/make-upper-case.js
@@ -0,0 +1,12 @@
+// Description:
+// Write a function which converts the input string to uppercase.
+
+// Solution:
+function makeUpperCase(str) {
+    return str.toUpperCase();
+}
+
+// Tests:
+console.log(makeUpperCase("hello"));
+console.log(makeUpperCase("hello world"));
+console.log(makeUpperCase("hello world !"));
