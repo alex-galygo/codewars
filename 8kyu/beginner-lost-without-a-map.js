57d11f81f68206ad7611a7195d7db9b4fb0e1292
Alex Galygo <alex.galygo.94@gmail.com>
2023-11-30 18:55:37 +0300

    "Added 'maps' function in beginner-lost-without-a-map.js
    The 'maps' function doubles the value of all integers in a given array. A new file, 'beginner-lost-without-a-map.js', has been introduced to host this function. Included in the file is a set of test cases to ensure the function performs as expected."

---
+++ 8kyu/beginner-lost-without-a-map.js
@@ -0,0 +1,16 @@
+// Description:
+// Given an array of integers, return a new array with each value doubled.
+//
+// For example:
+//
+// [1, 2, 3] --> [2, 4, 6]
+
+// Solution
+
+function maps(x) {
+    return x.map((num) => num * 2);
+}
+
+// Tests
+
+console.log(maps([1, 2, 3])); // [2, 4, 6]
