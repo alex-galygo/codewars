64c1c7f605c2292f742f98a704e9e93ad8219a00
Alex Galygo <alex.galygo.94@gmail.com>
2023-11-30 18:50:04 +0300

    "Add 'rowSumOddNumbers' function for finding sum of odd numbers
    Added a new JavaScript file '7kyu/sum-of-odd-numbers.js' that contains a 'rowSumOddNumbers' function. This function is to solve a coding exercise which requires finding a sum of odd numbers based on a pattern described in a provided triangle of consecutive odd numbers."

---
+++ 7kyu/sum-of-odd-numbers.js
@@ -0,0 +1,12 @@
+// Description:
+
+// Given the triangle of consecutive odd numbers:
+
+// Solution
+
+function rowSumOddNumbers(n) {
+    return Math.pow(n, 3);
+}
+
+// Tests
+console.log(rowSumOddNumbers(1)) // 1
